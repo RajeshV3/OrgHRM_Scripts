d5fb354513e47c54422abf00ad7393826cb0cda6
HP <HP@LAPTOP-LT413B6N>
2022-06-04 16:28:07 +0530

    updated test

--- OrgHrm/src/testcases/Logintestcase.java
+++ OrgHrm/src/testcases/Logintestcase.java
@@ -5,6 +5,7 @@ public class Logintestcase {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
  System.out.println("testing github");
+ System.out.println("testing github2");
 	}
 
 }
