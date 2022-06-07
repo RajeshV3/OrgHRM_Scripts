c356e91c0cf5eb9541762f6624fa19fe301255ab
HP <HP@LAPTOP-LT413B6N>
2022-06-07 21:29:38 +0530

    math

--- OrgHrm/src/testcases/Logintestcase.java
+++ OrgHrm/src/testcases/Logintestcase.java
@@ -4,8 +4,14 @@ public class Logintestcase {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- System.out.println("testing github");
- System.out.println("testing github2");
+	for(int i=1;i<101;i++){
+		System.out.println(i);
+		if(i%2==0){
+			System.out.println(i);
+		}
+		
+		
 	}
-
-}
+	}
+ 
+	}
